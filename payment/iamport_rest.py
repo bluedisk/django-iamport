20c6d6fef9ef734c21f7fae384bf359822ac64f8
bluedisk <bluedisk@gmail.com>
2019-05-21 16:25:30 +0900

    certification rest api support

--- payment/iamport_rest.py
+++ payment/iamport_rest.py
@@ -1,14 +1,18 @@
+from pprint import pprint
+
 import requests
 from django.conf import settings
 
+IAMPORT_API_ENDPOINT = 'https://api.iamport.kr/'
+
+IAMPORT_API_AUTH = '/users/getToken'
+IAMPORT_API_PREPARE = '/payments/prepare'
+IAMPORT_API_RESULT = '/payments/'
+IAMPORT_API_CANCEL = '/payments/cancel'
+IAMPORT_API_CERTIFICATION = '/certifications/'
+
 
 class IamportRest:
-    api_base = 'https://api.iamport.kr/'
-    api_auth = '/users/getToken'
-    api_prepare = '/payments/prepare'
-    api_result = '/payments/'
-    api_cancel = '/payments/cancel'
-
     token = ''
 
     class WrongStatus(Exception):
@@ -20,7 +24,9 @@ class IamportRest:
             return None
 
         result = data.json()
-        print(result)
+        if settings.DEBUG:
+            pprint(result)
+
         if result['code'] != 0:
             print("IAMPORT ERROR: " + result['message'])
             return None
@@ -28,7 +34,7 @@ class IamportRest:
         return result['response']
 
     def auth(self):
-        res = requests.post(self.api_base + self.api_auth, json={
+        res = requests.post(IAMPORT_API_ENDPOINT + IAMPORT_API_AUTH, json={
             'imp_key': settings.PAYMENT_REST_KEY,
             'imp_secret': settings.PAYMENT_REST_SECRET
         })
@@ -43,7 +49,7 @@ class IamportRest:
         if not self.token:
             raise IamportRest.WrongStatus()
 
-        res = requests.get(self.api_base + self.api_result + imp_uid, headers={'Authorization': self.token})
+        res = requests.get(IAMPORT_API_ENDPOINT + IAMPORT_API_RESULT + imp_uid, headers={'Authorization': self.token})
         unpacked = IamportRest.unpack(res)
         if not unpacked:
             return False
@@ -54,7 +60,7 @@ class IamportRest:
         if not self.token:
             raise IamportRest.WrongStatus()
 
-        res = requests.post(self.api_base + self.api_prepare, json={
+        res = requests.post(IAMPORT_API_ENDPOINT + IAMPORT_API_PREPARE, json={
             'merchant_uid': uid,
             'amount': amount
         }, headers={'Authorization': self.token})
@@ -71,7 +77,7 @@ class IamportRest:
         if not self.token:
             raise IamportRest.WrongStatus()
 
-        res = requests.post(self.api_base + self.api_cancel, json={
+        res = requests.post(IAMPORT_API_ENDPOINT + IAMPORT_API_CANCEL, json={
             'imp_uid': imp_uid,
             'merchant_uid': merchant_uid,
             'reason': reason
@@ -81,3 +87,16 @@ class IamportRest:
             return False
 
         return True
+
+    def certification(self, imp_uid):
+        if not self.token:
+            raise IamportRest.WrongStatus()
+
+        res = requests.get(IAMPORT_API_ENDPOINT + IAMPORT_API_CERTIFICATION + imp_uid,
+                           headers={'Authorization': self.token})
+
+        unpacked = IamportRest.unpack(res)
+        if not unpacked:
+            return False
+
+        return unpacked
